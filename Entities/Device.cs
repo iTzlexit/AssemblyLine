153ba9d2d52096547a9462f7976a3ae71b0bf900
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-11-28 12:00:18 +0200

    Setting up the Architecture

---
+++ Entities/Device.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Device
+    {
+        public int DeviceId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DeviceType DeviceType { get; set; }
+
+
+    }
+
+
+
+    public enum DeviceType
+    {
+        BarcodeScanner,
+        Printer,
+        Camera,
+        SocketTray
+    }
+}
---
+++ Entities/Operation.cs
@@ -0,0 +1,12 @@
+﻿namespace Entities
+{
+    public class Operation
+    {
+        public int OperationId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int OrderInWhichToPerform { get; set; }
+        public byte[] ImageData { get; set; } = new byte[0];
+
+        public Device Device { get; set; } = new Device(); // completes the operation 
+    }
+}
