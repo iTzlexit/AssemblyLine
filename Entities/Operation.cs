e7fb8dbd769afad49ee20e21ba0208bf7bddaad9
lexit <110407145+iTzlexit@users.noreply.github.com>
2023-11-28 14:52:32 +0200

    Implemented the in memory database for Operation and devices

--- Entities/Operation.cs
+++ Entities/Operation.cs
@@ -5,8 +5,11 @@
         public int OperationId { get; set; }
         public string Name { get; set; } = string.Empty;
         public int OrderInWhichToPerform { get; set; }
-        public byte[] ImageData { get; set; } = new byte[0];
+        public byte[] ImageData { get; set; } = new byte[0]; //store image of item being manufatured 
 
+        // Foreign key
+        public int DeviceId { get; set; }
+        // Navigation property
         public Device Device { get; set; } = new Device(); // completes the operation 
     }
 }
--- Plugins.InMemory/MockDb.cs
+++ Plugins.InMemory/MockDb.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,41 @@ using System.Threading.Tasks;
 
 namespace Plugins.InMemory
 {
-    public static class MockDb
+    public class MockDb
     {
+        public List<Device> Devices { get; private set; }
+        public List<Operation> Operations { get; private set; }
+
+        public MockDb()
+        {
+            InitializeDevices();
+            InitializeOperations();
+        }
+
+        private void InitializeDevices()
+        {
+            Devices = new List<Device>
+            {
+                new Device { DeviceId = 1, Name = "Scanner 1", DeviceType = DeviceType.BarcodeScanner },
+                new Device { DeviceId = 2, Name = "Printer A", DeviceType = DeviceType.Printer },
+                new Device { DeviceId = 3, Name = "Camera X", DeviceType = DeviceType.Camera },
+                new Device { DeviceId = 4, Name = "Socket Tray Z", DeviceType = DeviceType.SocketTray }
+
+            };
 
 
+        }
 
+        private void InitializeOperations()
+        {
+            Operations = new List<Operation>
+            {
+                new Operation { OperationId = 1, Name = "Scan Items", OrderInWhichToPerform = 1, ImageData = new byte[0], DeviceId = 1 },
+                new Operation { OperationId = 2, Name = "Print Labels", OrderInWhichToPerform = 2, ImageData = new byte[0], DeviceId = 2 },
+                new Operation { OperationId = 3, Name = "Capture Images", OrderInWhichToPerform = 3, ImageData = new byte[0], DeviceId = 3},
+                new Operation { OperationId = 4, Name = "Organize Trays", OrderInWhichToPerform = 4, ImageData = new byte[0], DeviceId = 4 }
+            };
 
-
+        }
     }
 }
